c79ea2e2ac9481e5d0cc784a2517586a77dec9f4
pavelKek <pavel52c@gmail.com>
2022-09-26 23:05:12 +0300

    firstLab

---
+++ lab1.0/lab1.0.cpp
@@ -0,0 +1,135 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+#include <fstream>
+#include <algorithm>
+using namespace std;
+
+vector<string> splitBySpace(const string str) {
+	stringstream strStream(str);
+	string vertice;
+	vector<string> vertices;
+	while (getline(strStream, vertice, ' ')) {
+		vertices.push_back(vertice);
+	}
+	return vertices;
+}
+
+void printVector(vector<string> vect) {
+	for (int i = 0; i < vect.size(); i++) {
+		cout << vect[i] << ' ';
+	}
+	cout << '\n';
+}
+
+bool findInVector(vector<string> line, string value) {
+	for (string str : line) {
+		if (value == str)
+			return true;
+	}
+	return false;
+}
+
+void printLs(map<string, vector<string>>  graph, vector<string> vertices, string outputFile) {
+	ofstream output;
+	output.open(outputFile);
+
+	if (output.is_open()) {
+		vector<string> value;
+		for (string vertice : vertices) {
+			output << vertice << ": ";
+
+			auto pointer = graph.find(vertice);
+			if (pointer != graph.end()) {
+				value = graph.find(vertice)->second;
+				for (string s : value)
+					output << s << " ";
+			}
+			else {
+				output << " ";
+			}
+			output << endl;
+		}
+		output << endl;
+	}
+}
+
+void printMx(map<string, vector<string>>  graph, vector<string> vertices, string fileName) {
+	ofstream output;
+	output.open(fileName);
+	if (output.is_open()) {
+		vector<string> value;
+		for (string verticeOutside : vertices) {
+			auto pointer = graph.find(verticeOutside);
+			string res = "";
+			if (pointer != graph.end()) {
+				value = graph.find(verticeOutside)->second;
+				for (string verticeInside : vertices) {
+					if (find(value.begin(), value.end(), verticeInside) != value.end()) {
+						res += "1 ";
+					}
+					else
+						res += "0 ";
+				}
+			}
+			else {
+				for (string verticeInside : vertices)
+					res += "0 ";
+			}
+			output << res << endl;
+		}
+	}
+}
+
+
+void insertToMap(map<string, vector<string>>& graph, vector<string> value) {
+	
+	auto pointer = graph.find(value[0]);
+	if (pointer != graph.end()) {
+		auto end = graph[value[0]].cend();
+		string addedValue = value[1];
+		graph[value[0]].insert(end, addedValue);
+	}
+	else {
+		vector<string> values;
+		values.push_back(value[1]);
+		graph.insert({ value[0], values });
+	}
+}
+
+void addToMap(map<string, vector<string>>& graph, string line) {
+	vector<string> value = splitBySpace(line);
+
+	insertToMap(graph, value);
+	reverse(value.begin(), value.end());
+
+	insertToMap(graph, value);
+}
+
+int main(int argc, char* argv[])
+{
+	map<string, vector<string>> graph;
+	
+	ifstream input(argv[1]);
+	string line;
+	
+	vector<string> newLine;
+	vector<string> vertices;
+
+
+	if (input.is_open()) {
+		getline(input, line);
+		vertices = splitBySpace(line);
+
+		while (getline(input, line)) {
+			addToMap(graph, line);
+		}
+	}
+
+	printLs(graph, vertices, argv[2]);
+	printMx(graph, vertices, argv[3]);
+
+	return 0;
+}
